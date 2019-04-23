9f53a11abfa12ae6bd4340b3f6671ccdf54f57c4
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-04-23 12:09:41 -0700

    Fixing the Navbar and working on the First Time Page: Set up a baseline 'banner' and made the navbar sticky

--- client/src/components/FirstTime/FirstTime.js
+++ client/src/components/FirstTime/FirstTime.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import "./FirstTime.css";
 
 const FirstTime = () => {
-  return ( <h3>First Time</h3> );
+  return (
+    <div className = "ft-component-wrapper">
+
+      <div className = "background-holder">
+
+        <div className = "background-text">
+          <h1 className = "background-title">First Time ?</h1>
+          <p className = "background-description">Blah blah</p>
+        </div>
+
+        <div className = "background-color-overlay"></div>
+      </div>
+    </div>
+  );
 }
  
 export default FirstTime;
