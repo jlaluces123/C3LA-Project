eb2ceb22c1b5d0b3aabf91ac804b69febc957ccf
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-04-23 11:24:48 -0700

    Navigation: Base Navigation Styling looking alright

---
+++ client/src/components/FirstTime/FirstTime.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FirstTime = () => {
+  return ( <h3>First Time</h3> );
+}
+ 
+export default FirstTime;
--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -11,6 +11,7 @@ import About from "../About/About.js";
 import Locations from "../Locations/Locations.js"
 import Sermons from "../Sermons/Sermons.js"
 import Home from "../Home/Home.js"
+import FirstTime from "../FirstTime/FirstTime.js";
 import NextSteps from "../NextSteps/NextSteps.js"
 import Ministries from "../Ministries/Ministries.js"
 import Give from "../Give/Give.js"
@@ -43,18 +44,20 @@ class Navigation extends React.Component {
     return (
       <div className = "navigation-component-wrapper">
         <ul className = "main-nav-wrapper">
-          <NavLink to = "/vision">About</NavLink>
-          <NavLink to = "/locations">Locations</NavLink>
-          <NavLink to = "/sermons">Sermons</NavLink>
-          <NavLink exact to = "/">Home</NavLink>
-          <NavLink to = "/next-steps">Next Steps</NavLink>
-          <NavLink to = "/ministries">Ministries</NavLink>
-          <NavLink to = "/give">Give</NavLink>
+          <NavLink to = "/vision" className = "nav-link">About</NavLink>
+          <NavLink to = "/locations" className = "nav-link">Locations</NavLink>
+          <NavLink to = "/sermons" className = "nav-link">Sermons</NavLink>
+          <NavLink exact to = "/" className = "nav-link">Home</NavLink>
+          <NavLink exact to = "/first-time" className = "nav-link">First Time</NavLink>
+          <NavLink to = "/next-steps" className = "nav-link">Next Steps</NavLink>
+          <NavLink to = "/ministries" className = "nav-link">Ministries</NavLink>
+          <NavLink to = "/give" className = "nav-link">Give</NavLink>
         </ul>
         <Route exact path = "/vision" component = {About} />
         <Route exact path = "/locations" component = {Locations} />
         <Route exact path = "/sermons" component = {Sermons} />
         <Route exact path = "/" component = {Home} />
+        <Route exact path = "/first-time" component = {FirstTime} />
         <Route exact path = "/next-steps" component = {NextSteps} />
         <Route exact path = "/ministries" component = {Ministries} />
         <Route exact path = "/give" component = {Give} />
