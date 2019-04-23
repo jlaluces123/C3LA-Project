83310b816a4f9f62a0d3a76814a5afa4613f0252
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-04-23 11:59:43 -0700

    Google Fonts + Refactoring: Added Raleway font as well as moved the Route tag to appJS file

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import { Route } from "react-router-dom";
+
 import Navigation from "./components/Navigation/Navigation.js";
+/* Components */
+import About from "./components/About/About.js";
+import Locations from "./components/Locations/Locations.js"
+import Sermons from "./components/Sermons/Sermons.js"
+import Home from "./components/Home/Home.js"
+import FirstTime from "./components/FirstTime/FirstTime.js";
+import NextSteps from "./components/NextSteps/NextSteps.js"
+import Ministries from "./components/Ministries/Ministries.js"
+import Give from "./components/Give/Give.js"
 
 function App() {
   return (
@@ -9,6 +20,15 @@ function App() {
       <header className="App-header">
         <Navigation />
       </header>
+
+      <Route exact path = "/vision" component = {About} />
+      <Route exact path = "/locations" component = {Locations} />
+      <Route exact path = "/sermons" component = {Sermons} />
+      <Route exact path = "/" component = {Home} />
+      <Route exact path = "/first-time" component = {FirstTime} />
+      <Route exact path = "/next-steps" component = {NextSteps} />
+      <Route exact path = "/ministries" component = {Ministries} />
+      <Route exact path = "/give" component = {Give} />
     </div>
   );
 }
--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -3,18 +3,7 @@ import "./Navigation.css";
 // import c3la from "../../assets/c3la-logo-black.png";
 
 /* Dependencies */
-import { NavLink, Route } from "react-router-dom";
-
-
-/* Components */
-import About from "../About/About.js";
-import Locations from "../Locations/Locations.js"
-import Sermons from "../Sermons/Sermons.js"
-import Home from "../Home/Home.js"
-import FirstTime from "../FirstTime/FirstTime.js";
-import NextSteps from "../NextSteps/NextSteps.js"
-import Ministries from "../Ministries/Ministries.js"
-import Give from "../Give/Give.js"
+import { NavLink } from "react-router-dom";
 
 
 // const aboutNav = {
@@ -45,22 +34,11 @@ class Navigation extends React.Component {
       <div className = "navigation-component-wrapper">
         <ul className = "main-nav-wrapper">
           <NavLink to = "/vision" className = "nav-link">About</NavLink>
-          <NavLink to = "/locations" className = "nav-link">Locations</NavLink>
-          <NavLink to = "/sermons" className = "nav-link">Sermons</NavLink>
+          <NavLink to = "/locations" className = "nav-link">Locations</NavLink>          
           <NavLink exact to = "/" className = "nav-link">Home</NavLink>
           <NavLink exact to = "/first-time" className = "nav-link">First Time</NavLink>
           <NavLink to = "/next-steps" className = "nav-link">Next Steps</NavLink>
-          <NavLink to = "/ministries" className = "nav-link">Ministries</NavLink>
-          <NavLink to = "/give" className = "nav-link">Give</NavLink>
-        </ul>
-        <Route exact path = "/vision" component = {About} />
-        <Route exact path = "/locations" component = {Locations} />
-        <Route exact path = "/sermons" component = {Sermons} />
-        <Route exact path = "/" component = {Home} />
-        <Route exact path = "/first-time" component = {FirstTime} />
-        <Route exact path = "/next-steps" component = {NextSteps} />
-        <Route exact path = "/ministries" component = {Ministries} />
-        <Route exact path = "/give" component = {Give} />
+        </ul>        
       </div>
     );
   }
